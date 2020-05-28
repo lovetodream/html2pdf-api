b32d118c31c3d4e2101269811af4413878b4144d
Timo Zacherl <t.zacherl@icloud.com>
2020-05-28 20:26:18 +0200

    Testing setup

---
+++ src/tests/routes.test.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const request = require('supertest');
+const app = require('../index');
+describe('API Endpoints', () => {
+  it('should check if api is online', async () => {
+    const res = await request.agent(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('online');
+  });
+
+  it('should send html string/file and return pdf', async () => {
+    const res = await request
+      .agent(app)
+      .post('/htmlToPdf')
+      .send('<html><body><h1>Test</h1></body></html>');
+    expect(res.statusCode).toEqual(200);
+    expect(res.headers).toHaveProperty('content-type', 'application/pdf');
+  });
+});
