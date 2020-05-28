1cb565977f531f5892994187da03ea3f681154f7
Timo Zacherl <t.zacherl@icloud.com>
2020-05-28 20:43:15 +0200

    Fixed test

--- src/tests/routes.test.js
+++ src/tests/routes.test.js
@@ -3,18 +3,22 @@
 const request = require('supertest');
 const app = require('../index');
 describe('API Endpoints', () => {
-  it('should check if api is online', async () => {
+  it('should check if api is online', async (done) => {
     const res = await request.agent(app).get('/');
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty('online');
+    app.close();
+    done();
   });
 
-  it('should send html string/file and return pdf', async () => {
+  it('should send html string/file and return pdf', async (done) => {
     const res = await request
       .agent(app)
       .post('/htmlToPdf')
       .send('<html><body><h1>Test</h1></body></html>');
     expect(res.statusCode).toEqual(200);
     expect(res.headers).toHaveProperty('content-type', 'application/pdf');
+    app.close();
+    done();
   });
 });
